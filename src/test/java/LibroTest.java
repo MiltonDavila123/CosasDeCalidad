adc0ddb4d730c50ded73414ef10dc236cd0f88f8
MiltonDavila2 <miltondaviladt@gmail.com>
2025-11-24 21:22:52 -0500

    cosas calidad

---
+++ src/test/java/LibroTest.java
@@ -0,0 +1,28 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LibroTest {
+
+    @Test
+    void testPrestarLibro() {
+        Libro libro = new Libro("1984", "Orwell");
+        libro.prestar();
+        assertFalse(libro.estaDisponible());
+    }
+
+    @Test
+    void testPrestarLibroNoDisponible() {
+        Libro libro = new Libro("1984", "Orwell");
+        libro.prestar();
+        assertThrows(IllegalStateException.class, libro::prestar);
+    }
+
+    @Test
+
+    void testPrestarLibroDevolver() {
+        Libro libro = new Libro("1984", "Orwell");
+        libro.prestar();
+        libro.devolver();
+        assertTrue(libro.estaDisponible());
+    }
+}
